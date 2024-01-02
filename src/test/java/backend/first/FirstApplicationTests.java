f9e05336feac67a4e08260c23ce9e73d0bbb26c4
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-02 15:11:13 +0530

    first commit

---
+++ src/test/java/backend/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
